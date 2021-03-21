4e1e8193e0411f8b8f9b638b042d08df031b9dc8
Marzela547 <marcelazelaya547@yahoo.com>
2021-03-21 04:23:16 -0600

    Diseños de pagina

--- src/components/BrandHeader/BrandHeader.js
+++ src/components/BrandHeader/BrandHeader.js
@@ -3,8 +3,8 @@ import Brand from "./Brand";
 import "./BrandHeader.css";
 function BrandHeader({ children }){
   return (
-    <header>
-      <Brand title="ToDo List App"></Brand>
+    <header className="menu bg-gradient-to-r from-gray-400 via-gray-200 to-gray-400 shadow-md text-2xl py-0">
+      <Brand title="Covid Api"></Brand>
       <NavBar></NavBar>
     </header>
   );
